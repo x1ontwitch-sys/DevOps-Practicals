463979c5bf5dc6536f82f0b53fcef823f05dd4e7
x1ontwitch-sys <x1ontwitch-sys@users.noreply.github.com>
2026-05-29 12:25:28 +0530

    Error

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 EXPOSE 3000
-
+expose 3123
 # Use node directly instead of npm start
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -8,3 +8,18 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log(`Server is running on http://localhost:3000`);
 });
+
+app.get('/users', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            name: "John Doe",
+            age: 30,
+        },
+        {
+            id: 2,
+            name: "Jane Doe",
+            age: 25,
+        },
+    ])
+})
